4bc8ef7f2f530a00ed3fa239b5ed389472f33cc0
Jonathan Tsai <modopo@gmail.com>
2023-01-27 14:08:20 -0800

    multiplied array of nth length

--- js/app.js
+++ js/app.js
@@ -134,9 +134,9 @@ function multiplyArray(multArr) { //eslint-disable-line
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-testMultiplyArray(testArray);
+//testMultiplyArray(testArray);
 
-// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
+// Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop.
 
 // You're done! Submit the link to the repo following the instructions in Canvas. Or, try out the stretch goal below...
 
@@ -158,10 +158,23 @@ Test this function by hand in the console to get it working, and when you think 
 let testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
+  let result = [];
+
+  let prodResult = dynamicArray.reduce((accum, current) => {
+    return multiply(accum, current)[0];
+  });
+
+  let prodString = `The numbers ${dynamicArray.join(',')} have a product of ${prodResult}.`;
+
+  result.push(prodResult, prodString);
+
+  console.log(result);
+
+  return result;
 
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyAnyArray(testDynamicArray);
+testMultiplyAnyArray(testDynamicArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. You're done! Submit the link to the repo following the instructions in Canvas.
